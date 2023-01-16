6908e181695de992fb20c82d4aa2563423278516
Nartova Ksenia <xenianartova@yandex.ru>
2023-01-16 18:44:35 +0800

    1 rev

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const routes = require('./routes/index');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 app.use(express.json());
